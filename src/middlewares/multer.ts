89abe2acd8453ac319b7a2a031e3208ac85661b7
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-23 17:02:48 +0300

    Create multer middleware to handle file uploads to server

---
+++ src/middlewares/multer.ts
@@ -0,0 +1,27 @@
+import multer, { diskStorage } from 'multer';
+import { checkFileType } from '../utils/file';
+import fs from 'fs';
+
+const storage = diskStorage({
+	destination: function (req, file, cb) {
+		fs.stat(`${__dirname.slice(0, __dirname.length - 11)}/tmp/my-uploads`, (err, stat) => {
+			if(err) {
+				fs.mkdirSync(`${__dirname.slice(0, __dirname.length - 11)}/tmp/my-uploads`, { recursive: true });
+			} else if(stat.isDirectory()) {
+				cb(null, `${__dirname.slice(0, __dirname.length - 11)}/tmp/my-uploads`);
+			}
+		});
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now();
+		cb(null, file.fieldname + '-' + uniqueSuffix);
+	},
+	
+});
+
+export const csvUploads = multer({ 
+	storage,
+	fileFilter(req, file, callback) {
+		checkFileType(file, callback);
+	},
+});
