6e76832fd7baace9c925b75e37e9eea09b1ba1a1
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-25 12:33:50 +0300

    Hardcode env vars

--- dist/config/config.js
+++ dist/config/config.js
@@ -8,7 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const { MONGO_URI, JWT_SECRET, PORT, SMTP_HOST, SMTP_SENDER, SMTP_USERNAME, SMTP_PASSWORD, } = process.env;
 exports.settings = {
-    database: MONGO_URI || 'mongodb://127.0.0.1:27017',
+    database: MONGO_URI || 'mongodb+srv://elsafricaltd:Ikk@29250072@cluster0.iswzraw.mongodb.net/?retryWrites=true&w=majority',
     secret: JWT_SECRET || '$2a$10$zIzi0OAJgG5TLMbU1yq0YON8.YSU.GMr/ktsOQZ3XCVHXnztpb4aa',
     PORT,
     SMTP_HOST,
