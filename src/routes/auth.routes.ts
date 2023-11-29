1c5d1dddc74efb66d587bc5413898b82e38a57ad
Timothy Njoroge <timothy.njoroge@jithvar.com>
2023-11-29 21:42:24 +0300

    Create new routes for auth controllers

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,16 +1,60 @@
 import { Router } from 'express';
 import * as controllers from '../controllers/auth.controller';
+import { body } from 'express-validator';
 
 const router = Router();
 
 router.post(
 	'/sign_up',
+	body([
+		'firstName',
+		'lastName',
+		'phoneNo',
+		'password',
+		'email'
+	])
+		.notEmpty(),
 	controllers.signUp
 );
 
 router.post(
 	'/sign_in',
+	body([
+		'password',
+		'email'
+	])
+		.notEmpty(),
 	controllers.login
+);
+
+router.post(
+	'/change_password',
+	body([
+		'password',
+		'newPassword',
+		'confirmPassword',
+	])
+		.notEmpty(),
+	controllers.changePassword
+);
+
+router.post(
+	'/reset_password',
+	body([
+		'email'
+	])
+		.notEmpty(),
+	controllers.forgotPassword
+);
+
+router.post(
+	'/new_password',
+	body([
+		'password',
+		'sentToken'
+	])
+		.notEmpty(),
+	controllers.newPassword
 );
 
 export default router;
